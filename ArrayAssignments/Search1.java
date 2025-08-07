cfdd1e3d68d5fd7e5ba3b7202cbc08b9707178a8
Piyush Raj <piyush004.raj@gmail.com>
2025-08-08 02:01:30 +0530

    There is an integer array nums sorted in ascending order (with distinct values).Prior to being passed to your function, nums is possibly rotated at an unknown pivot  index k (1  <=  k  <  nums.length)  such  that  the  resulting  array  is [nums[k], nums[k+1],   ...,   nums[n-1], nums[0],   nums[1],   ...,   nums[k-1]] (0-indexed).   For   example, [0,1,2,4,5,6,7] might        be        rotated        at        pivot        index 3   and become [4,5,6,7,0,1,2].Given the array nums after the possible rotation and an integer target, returnthe index oftarget if it is in nums, or -1   if it is not in nums.You must write an algorithm with O(log n) runtime complexity

---
+++ ArrayAssignments/Search1.java
@@ -0,0 +1,23 @@
+package ArrayAssignments;
+
+public class Search1 {
+
+    public static int searchKey(int arr[],int target)
+    {
+        for(int i = 0;i<arr.length;i++)
+        {
+            if(target==arr[i])
+            {
+                return i;
+            }
+        }
+        return -1;
+    }
+    public static void main(String[] args) {
+        int arr1[] = {4, 5, 6, 7, 0, 1, 2};
+        int arr2[] = {1};
+        System.out.println( searchKey(arr1, 3));
+
+    }
+    
+}
