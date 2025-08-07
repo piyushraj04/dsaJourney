a4cf5ee5dd271addea2b4b6a92f7860f22e68288
Piyush Raj <piyush004.raj@gmail.com>
2025-08-08 02:21:17 +0530

    Given an integer array nums, return all the triplets [nums[i], nums[j],  nums[k]] such that i != j, i != k, and j != k, and nums[i] + nums[j] + nums[k] == 0.Notice that the solution set must not contain duplicate triplets.

---
+++ ArrayAssignments/DistinctsTriplets.java
@@ -0,0 +1,26 @@
+package ArrayAssignments;
+
+public class DistinctsTriplets {
+    public static void triplets(int arr[])
+    {
+        int temp[] = new int[3];
+        for(int i = 0;i<arr.length;i++)
+        {
+            for(int j = i+1;j<arr.length;j++)
+            {
+                for(int k = j+1;k<arr.length;k++)
+                {
+                    if(((arr[i]+arr[j]+arr[k])==0))
+                    {
+                        System.out.println(arr[i]+" "+arr[j]+" "+arr[k]);
+                    }
+                }
+            }
+        }
+    }
+    public static void main(String[] args) {
+        int arr [] = {-1, 0,  1, 2, -1, -4};
+        triplets(arr);
+    }
+    
+}
