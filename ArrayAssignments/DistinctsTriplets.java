1b0f16fbd145ff4f8490d140c867c1e712e750a3
Piyush Raj <piyush004.raj@gmail.com>
2025-08-08 21:23:31 +0530

    update

--- ArrayAssignments/DistinctsTriplets.java
+++ ArrayAssignments/DistinctsTriplets.java
@@ -3,17 +3,19 @@ package ArrayAssignments;
 public class DistinctsTriplets {
     public static void triplets(int arr[])
     {
-        int temp[] = new int[3];
         for(int i = 0;i<arr.length;i++)
         {
             for(int j = i+1;j<arr.length;j++)
             {
                 for(int k = j+1;k<arr.length;k++)
                 {
-                    if(((arr[i]+arr[j]+arr[k])==0))
+                    if(i!=j && i!=k && j!=k){
+                        if(((arr[i]+arr[j]+arr[k])==0))
                     {
                         System.out.println(arr[i]+" "+arr[j]+" "+arr[k]);
                     }
+                    }
+                    
                 }
             }
         }
