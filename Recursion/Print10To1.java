5cc3dea7331a2086deb4408487739fd1a4670530
Piyush Raj <piyush004.raj@gmail.com>
2025-08-15 22:55:51 +0530

    print 10 to 1

--- Recursion/Print10To1.java
+++ Recursion/Print10To1.java
@@ -1,10 +1,19 @@
 package Recursion;
 
 public class Print10To1 {
+    public static int print(int n)
+    {
+        if(n==1)
+        {
+            return 1;
+        }
+        System.out.println(n);
+        return print(n-1);
+    }
    
     
     public static void main(String[] args) {
-        
+        System.out.println(print(10));
     }
 
     
