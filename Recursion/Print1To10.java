e91300bd51bf76aabc75c5b1e565018201b6a313
Piyush Raj <piyush004.raj@gmail.com>
2025-08-16 00:30:14 +0530

    reverse

--- Recursion/Print1To10.java
+++ Recursion/Print1To10.java
@@ -10,8 +10,31 @@ public class Print1To10 {
         System.out.println(n);
         return print(n+1);
     }
+
+    private static void printInc(int n)
+    {
+        if(n==10)
+        {
+            System.out.println(n);
+            return;
+        }
+        System.out.println(n);
+        printInc(n+1);
+    }
+
+    static void printInc2(int n)
+    {
+        if(n==1)
+        {
+            System.out.print(n+" ");
+            return;
+        }
+        printInc2(n-1);
+        System.out.print(n+" ");
+    }
     public static void main(String[] args) {
-        System.out.println(print(1));
+        //System.out.println(print(1));
+        printInc2(10);
     }
     
 }
