35113a95576c70f04ad188ccaf7b498170a48b22
Piyush Raj <piyush004.raj@gmail.com>
2025-08-18 21:22:01 +0530

    tiling problem

---
+++ Recursion/TilingProblem.java
@@ -0,0 +1,23 @@
+package Recursion;
+
+public class TilingProblem {
+    public static int tilingProblem(int n)
+    {
+        //base condition
+        if(n == 0 || n == 1)
+        {
+            return 1;
+        }
+        //vertically
+        int fnm1 = tilingProblem(n-1);
+        //horizontally
+        int fnm2 = tilingProblem(n-2);
+        //total ways
+        return fnm1 + fnm2;
+    }
+    public static void main(String[] args) {
+        System.out.println(tilingProblem(4));
+        
+    }
+    
+}
