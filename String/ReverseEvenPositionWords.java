1a8da3d9322d2c8dc583f7910529ed53a11f294c
Piyush Raj <piyush004.raj@gmail.com>
2025-08-22 19:32:36 +0530

    ReverseEvenPositionWords

---
+++ String/ReverseEvenPositionWords.java
@@ -0,0 +1,27 @@
+package String;
+
+public class ReverseEvenPositionWords {
+    private static String reverse(String word)
+    {
+        String rev = "";
+        for(int i = word.length()-1;i >= 0;i--)
+        {
+            rev+=word.charAt(i);
+        }
+        return rev;
+    }
+    private static void reverseEvenPositionWords(String sentence)
+    {
+        String[] str = sentence.split(" ");
+        for(int i = 0; i < str.length;i++)
+        {
+           System.out.print((i%2==0)?str[i]+" ":reverse(str[i]) + " ");
+        }
+    }
+    public static void main(String[] args) {
+        String s = "hello hi bye how are you";
+        reverseEvenPositionWords(s);
+        
+    }
+    
+}
