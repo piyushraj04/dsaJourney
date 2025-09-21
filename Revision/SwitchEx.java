b6f0fa0e5957c4ddf728546a7bd77467d55ff336
Piyush Raj <piyush004.raj@gmail.com>
2025-09-21 14:09:39 +0530

    revision

---
+++ Revision/SwitchEx.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+public class SwitchEx {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the number 1 for 'Sunday' to 7 for 'Saturday':-");
+        int day = sc.nextInt();
+        switch(day)
+        {
+            case 1: System.out.println("Sunday");
+            break;
+            case 2: System.out.println("Monday");
+            break;
+            case 3:System.out.println("Tuesday");
+            break;
+            case 4:System.out.println("Wednesday");
+            break;
+            case 5:System.out.println("Thursay");
+            break;
+            case 6:System.out.println("Friday");
+            break;
+            case 7:System.out.println("Saturday");
+            break;
+            default:System.out.println("Invalid choice enter");
+        }
+
+    }
+}
---
+++ Revision/ternary.java
@@ -0,0 +1,10 @@
+public class ternary{
+    public static void main(String [] args)
+    {
+        int  a = 5;
+        int b = 8;
+        int c = 12;
+        System.out.println(a>b?a:b>c?b:c);
+        System.out.println(a>b?"Piyush":b>c?"Raj":"Mayank");
+    }
+}
